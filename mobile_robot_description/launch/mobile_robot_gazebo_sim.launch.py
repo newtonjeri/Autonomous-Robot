6878a1df91db8169b1dc257ef6886890edd84c10
newtonjeri <newtonkaris45@gmail.com>
2025-02-02 22:56:56 +0300

    Refactor mobile robot Gazebo launch file to include world argument and add diff drive spawner

--- mobile_robot_description/launch/mobile_robot_gazebo_sim.launch.py
+++ mobile_robot_description/launch/mobile_robot_gazebo_sim.launch.py
@@ -3,10 +3,11 @@ from launch import LaunchDescription
 from launch.substitutions import (
     Command,
     PathJoinSubstitution,
-    FindExecutable
+    FindExecutable,
+    LaunchConfiguration
 )
 from launch_ros.substitutions import FindPackageShare
-e
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import (
     get_package_share_directory,
@@ -14,8 +15,12 @@ from ament_index_python.packages import (
 )
 from launch.actions import (
     IncludeLaunchDescription,
+    DeclareLaunchArgument
 )
-from launch_ros.actions import Node
+from launch_ros.actions import (
+    Node,
+    SetParameter,
+)
 
 
 description_pkg = "mobile_robot_description"
@@ -24,10 +29,12 @@ xacro_filename = "mobile_robot.urdf.xacro"
 def generate_launch_description():
     # Path to xacro file
     xacro_file = os.path.join(get_package_share_directory(description_pkg), 'urdf', xacro_filename)
-    # Path to world file
-    world = os.path.join(get_package_share_directory(description_pkg), 'worlds', 'custom_world.sdf')
-    # Path to ROS gz_sim package
-    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    world_file = PathJoinSubstitution([description_pkg, "worlds", "custom_world.sdf"])
+    world_cfg = LaunchConfiguration("world")
+    declare_world_arg = DeclareLaunchArgument(
+        "world", default_value=["-r ", world_file], description="SDF world file"
+    )
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -43,6 +50,7 @@ def generate_launch_description():
             ),
         ]
     )
+
     robot_description = {"robot_description": robot_description_content}
 
     # Environment Variable
@@ -59,11 +67,17 @@ def generate_launch_description():
     )
 
     # gazebo
-    gz_cmd = IncludeLaunchDescription(
+    gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+            PathJoinSubstitution(
+                [
+                    get_package_share_directory("ros_gz_sim"),
+                    "launch",
+                    "gz_sim.launch.py",
+                ]
+            )
         ),
-        launch_arguments={'gz_args': ['-r ', world], 'on_exit_shutdown': 'true'}.items()
+        launch_arguments={"gz_args": world_cfg}.items(),
     )
     
     # gazebo_ros_spawner	
@@ -81,8 +95,30 @@ def generate_launch_description():
         output='screen',
     )
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    # Bridge
+    bridge_params = os.path.join(get_package_share_directory(description_pkg),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
     return LaunchDescription([
+        # Sets use_sim_time for all nodes started below (doesn't work for nodes started from Gazebo)
+        SetParameter(name="use_sim_time", value=True),
+        declare_world_arg,
         robot_state_publisher,
-        gz_cmd,
+        gz_sim,
         start_gazebo_ros_spawner_cmd,
+        diff_drive_spawner,
     ])
