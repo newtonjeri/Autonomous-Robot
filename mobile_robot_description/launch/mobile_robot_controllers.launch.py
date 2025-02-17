c4e64731fa438cc5057b4f3078245bba2596c986
newtonjeri <newtonkaris45@gmail.com>
2025-02-17 17:58:22 +0300

    Refactor mobile_robot_controllers.launch.py to simplify controller node configurations and remove unused entries for clarity

--- mobile_robot_description/launch/mobile_robot_controllers.launch.py
+++ mobile_robot_description/launch/mobile_robot_controllers.launch.py
@@ -6,27 +6,10 @@ from launch.actions import ExecuteProcess, DeclareLaunchArgument
 
 controllers_pkg = 'mobile_robot_description'
 def generate_launch_description():
-
-   
     use_sim_time = LaunchConfiguration("use_sim_time")
     declare_use_sim_time_arg = DeclareLaunchArgument(
         "use_sim_time", default_value="true", description="Use simulation time"
     )
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare(controllers_pkg),
-            "config",
-            "controllers.yaml",
-        ]
-    )
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_controllers, {use_sim_time: True}], 
-        output="both",
-    )
-        
     
     joint_state_broadcaster = Node(
         package="controller_manager",
@@ -35,61 +18,17 @@ def generate_launch_description():
         output='screen',
 
     )
-    load_forward_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'forward_velocity_controller'],
-        output='screen'
-    )
-
-    load_forward_position_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'forward_position_controller'],
-        output='screen'
-    )
-
-    # ackermann_steering_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-    #          'ackermann_steering_cont'],
-    #     output='screen'
-    # )
 
     ackermann_steering_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "ackermann_steering_cont",
-            "--param-file",
-            robot_controllers
-        ],
-        parameters=[{use_sim_time: True}],
-        output='screen',
+       package="controller_manager",
+       executable="spawner",
+       parameters=[{use_sim_time: True}],
+       arguments=["ackermann_steering_cont"],
     )
 
-    # ackermann_steering_controller = Node(
-    #    package="controller_manager",
-    #    executable="spawner",
-    #    parameters=[{use_sim_time: True}],
-    #    arguments=["ackermann_steering_cont"],
-    # )
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "diff_drive_controller",
-            "--param-file",
-            robot_controllers,
-            # "--controller-ros-args",
-            # "-r /diff_drive_controller/cmd_vel:=/cmd_vel",
-        ],
-        output='screen',
-    )
 
     return LaunchDescription([
         declare_use_sim_time_arg,
-        control_node,
         joint_state_broadcaster,
-        # load_forward_velocity_controller,
-        # load_forward_position_controller,
         ackermann_steering_controller,
-        # diff_drive_spawner,
     ])
